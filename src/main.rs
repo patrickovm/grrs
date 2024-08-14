7878588ed6ce725e30e20cff6da0852cea95758e
Patrick Monteiro <patrickom21@gmail.com>
2024-08-13 22:17:49 -0300

    feat: optimize file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::BufRead;
+
 use clap::Parser;
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -11,9 +13,11 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let content = std::fs::File::open(&args.path).expect("could not open file");
 
-    for line in content.lines() {
+    let reader = std::io::BufReader::new(content);
+    for line in reader.lines() {
+        let line = line.expect("could not read line");
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
