0bc85da3c89f627c2287f23b48f7c78b3622c6cc
Patrick Monteiro <patrickom21@gmail.com>
2024-08-15 21:31:22 -0300

    refactor: split code into library and binary targets

--- src/main.rs
+++ src/main.rs
@@ -15,25 +15,7 @@ fn main() -> Result<()> {
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
-    find_matches(&content, &args.pattern, &mut std::io::stdout());
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
-}
-
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
-    for line in content.lines() {
-        if line.contains(pattern) {
-            match writeln!(writer, "{}", line) {
-                Ok(_) => {}
-                Err(e) => eprintln!("Error writing to writer: {}", e),
-            }
-        }
-    }
-}
-
-#[test]
-fn find_a_match() {
-    let mut result = Vec::new();
-    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
-    assert_eq!(result, b"lorem ipsum\n");
 }
